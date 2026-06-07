081bb9e687f34fa60009ad4f705f2d43d1ace536
Nick Brice <nick.brice88@gmail.com>
2026-06-07 11:39:04 -0500

    feat: Add movement logic to move ships along maneuver templates.
    Supports all templates, placing the ship with its base aligned to the end of the template and its heading parallel with either the tangent vector at the end of the maneuver template, or rotated according to the special maneuver's definition (like the Tallon roll).

--- game/__init__.py
+++ game/__init__.py
@@ -1,3 +1,12 @@
 """
 Utilities necessary to read and update the game state.
 """
+
+from .geometry import Pose as Pose
+from .geometry import Radian as Radian
+from .geometry import Vector2 as Vector2
+from .geometry import forward_vector as forward_vector
+from .geometry import left_vector as left_vector
+from .geometry import vector_left_of as vector_left_of
+from .geometry import right_vector as right_vector
+from .geometry import vector_right_of as vector_right_of
