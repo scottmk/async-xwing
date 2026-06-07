410c816fc58162c0b43def835518c2609d66ffbf
Nick Brice <nick.brice88@gmail.com>
2026-06-07 11:21:47 -0500

    feat: Add Entity type to represent an object on the game board.
    Also adds a Pose type to represent where the object is on the game board and what direction it is facing.
    
    This is not fully complete as pygame.math.Vector2 is not msgspec compatible yet. Will update PR with explicit Vector2 serialization once general game serialization is committed to main.

---
+++ game/geometry/__init__.py
@@ -0,0 +1,21 @@
+from .geometry import (
+    Pose,
+    Radian,
+    Vector2,
+    forward_vector,
+    left_vector,
+    vector_left_of,
+    right_vector,
+    vector_right_of,
+)
+
+__all__ = [
+    'Pose',
+    'Radian',
+    'Vector2',
+    'forward_vector',
+    'left_vector',
+    'vector_left_of',
+    'right_vector',
+    'vector_right_of',
+]
---
+++ game/geometry/geometry.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from pygame.math import Vector2
+
+import math
+import msgspec
+
+type Radian = float
+
+
+class Pose(msgspec.Struct, kw_only=True, frozen=True):
+    """An object's position and rotation in a coordinate system."""
+
+    position: Vector2
+    """This object's position."""
+    rotation: Radian
+    """This object's rotation, in Radians. +x is 0. Positive values rotate counter-clockwise."""
+
+    @property
+    def forward_vector(self) -> Vector2:
+        """Returns a unit vector pointing in the direction of this object, based on its ''rotation''."""
+        return forward_vector(self.rotation)
+
+
+def forward_vector(angle: Radian) -> Vector2:
+    """Returns a unit vector pointing in the direction of the given angle."""
+    return Vector2(x=math.cos(angle), y=math.sin(angle)).normalize()
+
+
+def left_vector(angle: Radian) -> Vector2:
+    """Returns a unit vector pointing to the left of the given angle."""
+    return Vector2(x=-math.sin(angle), y=math.cos(angle)).normalize()
+
+
+def vector_left_of(vector: Vector2) -> Vector2:
+    """Returns a unit vector perpendicular to the given vector pointing to its left."""
+    left_vector = Vector2(x=-vector.y, y=vector.x)
+    # The provided vector may not be a normalized vector. Only normalize if needed since it's a slow operation.
+    if not left_vector.is_normalized():
+        left_vector.normalize_ip()
+    return left_vector
+
+
+def right_vector(angle: Radian) -> Vector2:
+    """Returns a unit vector pointing to the right of the given angle."""
+    return Vector2(x=math.sin(angle), y=-math.cos(angle)).normalize()
+
+
+def vector_right_of(vector: Vector2) -> Vector2:
+    """Returns a unit vector perpendicular to the given vector pointing to its right."""
+    right_vector = Vector2(x=vector.y, y=-vector.x)
+    # The provided vector may not be a normalized vector. Only normalize if needed since it's a slow operation.
+    if not right_vector.is_normalized():
+        right_vector.normalize_ip()
+    return right_vector
