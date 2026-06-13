888960308af22af39c89914912d10626fbba39ca
scottmk <michael.kelly.scott@gmail.com>
2026-06-13 03:03:08 -0700

    feat: added upgrade entries for every upgrade card and made necessary changes to catalog models accordingly

--- game/model/catalog/upgrade.py
+++ game/model/catalog/upgrade.py
@@ -7,6 +7,7 @@ from game.model import Faction
 from game.model.catalog.card_attr import (
     Ability,
     Action,
+    Arc,
     Card,
     ChargeType,
     ChargeValues,
@@ -19,10 +20,8 @@ class ShipStat(StrEnum):
     ATTACK = auto()
     AGILITY = auto()
     HULL = auto()
-    SHIELD = auto()
-    STANDARD_CHARGE_LIMIT = auto()
-    FORCE_CHARGE_LIMIT = auto()
-    ENERGY_CHARGE_LIMIT = auto()
+    SHIELDS = auto()
+    ENERGY = auto()
 
 
 class UpgradeType(StrEnum):
@@ -75,18 +74,37 @@ class BonusAttack(SpecialAttack):
 
 
 class UpgradeCard(Card, kw_only=True, frozen=True):
-    types: Annotated[
-        list[UpgradeType], msgspec.Meta(description='A list because types can be duplciated')
-    ]
+    type_: UpgradeType
+    slots: Annotated[
+        list[UpgradeType], msgspec.Meta(description='The slots required to equip this upgrade')
+    ] = []
     name: str
-    unique: bool
+    is_unique: bool
+    limit: int | None = None
     ability: Ability | None = None
     flavor_text: str | None = None
     charges: dict[ChargeType, ChargeValues] | None = None
     special_attacks: list[SpecialAttack] | None = None
-    bonus_attacks: list[BonusAttack] | None = None
+    huge_ship_bonus_attacks: Annotated[
+        list[BonusAttack] | None,
+        msgspec.Meta(
+            description='Special bonus attacks only for hardpoint upgrades, which are for Huge Ships. Different from regular bonus attacks, which are handled by automation strings.'
+        ),
+    ] = None
+    replacement_arc: Annotated[
+        Arc | None,
+        msgspec.Meta(description='Replaces the attack arc of the ship this is attached to'),
+    ] = None
     action_bar: list[Action] | None = None
     ship_stat_modifiers: dict[ShipStat, int] | None = None
+    bonus_slots: dict[UpgradeType, int] | None = None
+    bonus_keywords: set[Keyword] | None = None
     deployable_remote: Remote | None = None
     restrictions: set[UpgradeRestriction] | None = None
+    is_reverse: bool = False
+    reverse_side_id: str | None = None
     setup_automations: list[str] | None = None
+
+    def __post_init__(self):
+        if len(self.slots) == 0:
+            object.__setattr__(self, 'slots', [self.type_])
